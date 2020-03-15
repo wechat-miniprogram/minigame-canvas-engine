71828ecd7facd3742e8f508573af42387ab17712
yuanzm <1229084233@qq.com>
2020-03-15 20:26:09 +0800

    docs: fix docs deploy

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ rm -r docs
 mkdir docs
 cp  docsource/playground/index.html docs/playground.html
 cp  docsource/playground/app.js docs/app.js
-cp -r docsource/docs/.vuepress/dist docs
+cp -ri docsource/docs/.vuepress/dist/* docs/
