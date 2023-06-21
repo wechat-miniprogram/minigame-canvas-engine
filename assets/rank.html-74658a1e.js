4291e4ab20a6fe86cf06071d0edd9d9accf39fa6
yuanzm <yuanzm@users.noreply.github.com>
2023-06-21 03:51:37 +0000

    deploy: ec420187300315fb49f3ef67a63abd583bb6852a

--- assets/rank.html-74658a1e.js
+++
@@ -1,5 +0,0 @@
-import{_ as a,r as o,o as r,c as s,a as e,b as n,d as l}from"./app-1201fcce.js";const c={},i=e("h1",{id:"好友排行榜",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#好友排行榜","aria-hidden":"true"},"#"),n(" 好友排行榜")],-1),d={href:"https://developers.weixin.qq.com/minigame/dev/guide/open-ability/open-data.html",target:"_blank",rel:"noopener noreferrer"},h=e("iframe",{height:"609.4705810546875",style:{width:"100%"},scrolling:"no",title:"Layout RankList",src:"https://codepen.io/yuanzm/embed/QWZybox?default-tab=html%2Cresult&editable=true",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/yuanzm/pen/QWZybox">
-  Layout RankList</a> by yuanzm (<a href="https://codepen.io/yuanzm">@yuanzm</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1);function p(m,u){const t=o("ExternalLinkIcon");return r(),s("div",null,[i,e("p",null,[n("在微信小游戏里面，关于好友关系链能力最常见的场景就是拉取同玩好友排行榜，详情可见微信"),e("a",d,[n("文档"),l(t)]),n("。")]),h])}const f=a(c,[["render",p],["__file","rank.html.vue"]]);export{f as default};
