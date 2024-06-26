43830bdcca9b4568c031ccc7f85c407d72863caf
申飞龙ᯤ⁶ᴳ <oncwnuNia1MOLWigUUmMqe8XUzU4@git.weixin.qq.com>
2024-06-26 14:28:12 +0800

    增加单步刷新API

--- src/common/ticker.ts
+++ src/common/ticker.ts
@@ -12,6 +12,8 @@ export default class Ticker {
 
   private lastTime!: number;
 
+  private residueStep!:number; //剩余的执行步数，== 0 时候停止循环，小于0 不限制
+
   private update = () => {
     const time = Date.now();
     const deltaTime = time - this.lastTime;
@@ -38,7 +40,14 @@ export default class Ticker {
     }
 
     this.count += 1;
-    this.animationId = requestAnimationFrame(this.update);
+    this.residueStep --;
+    if(this.residueStep !== 0){
+      this.animationId = requestAnimationFrame(this.update);
+    }else{
+      this.animationId = null;
+      this.started = false;
+    }
+
   }
 
   cancelIfNeed() {
@@ -84,22 +93,32 @@ export default class Ticker {
   }
 
   start() {
-    if (!this.started) {
-      this.started = true;
-
-      this.lastTime = Date.now();
-
-      if (this.animationId === null && (this.cbs.length || this.innerCbs.length)) {
-        this.animationId = requestAnimationFrame(this.update);
-      }
-    }
+    this.frame(-1);
   }
 
   stop() {
     if (this.started) {
       this.started = false;
-
       this.cancelIfNeed();
     }
   }
+
+  /**
+   * 单步执行, 指定需要执行的帧数，用户改变数据后刷新使用，不需要一致执行循环
+   *
+   * @param step 0 不执行，负数，无限执行 正数执行指定桢后停止
+   */
+  frame(step:number){
+    if(this.started || step === 0){
+      return;
+    }
+    this.started = true;
+    this.lastTime = Date.now();
+    this.residueStep = step;
+    if (this.animationId === null && (this.cbs.length || this.innerCbs.length)) {
+      this.animationId = requestAnimationFrame(this.update);
+    }
+  }
+
+
 }
